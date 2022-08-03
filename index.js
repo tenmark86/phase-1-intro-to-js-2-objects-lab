a8800519be2f6fa02e9b143e4fff85266b8464a4
tenmark86e <tenmark86@gmail.com>
2022-08-03 10:10:53 +0300

    completed objects lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {
+    name: 'SamSpeedy',
+    streetAddress: 'Abuja Nigeria',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    
+};
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+};
